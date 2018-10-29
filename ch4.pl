806ed66b2192c018622dfc33fcc6adadda2e82db
Patrick Liu <lost.h@qq.com>
2018-10-30 00:26:02 +0800

    finish ch4

--- ch4.pl
+++ ch4.pl
@@ -85,4 +85,203 @@ print "\$b has a value.\n";
 }
 
 #die "fail\n" if $b == 10;
- die "fail\n" unless $b != 10;
+#die "fail\n" unless $b != 10;
+
+
+ #!/usr/bin/perl
+#forloop1.plx
+use warnings;
+use strict;
+my @array = (1, 3, 5, 7, 9);
+my $i;
+for $i (@array) {
+print "This element: $i\n";
+}
+
+
+#!/usr/bin/perl
+#forloop2.plx
+use warnings;
+use strict;
+my @array = (1, 3, 5, 7, 9);
+my $i;
+foreach $i (@array) {
+print "This element: $i\n";
+}
+
+#!/usr/bin/perl
+#forloop3.plx
+use warnings;
+use strict;
+my @array = (1, 3, 5, 7, 9);
+foreach my $i (@array) {
+print "This element: $i\n";
+}
+
+#!/usr/bin/perl
+#forloop4.plx
+use warnings;
+use strict;
+my @array = (1, 3, 5, 7, 9);
+my $i="Hello there";
+foreach $i (@array) {
+print "This element: $i\n";
+}
+print "All done: $i\n";
+
+
+my @array = qw(the quick brown fox ran over the lazy dog);
+for (6, 3, 8, 2, 5, 4, 0, 7) { print "$array[$_] ";
+}
+print "\n";
+
+
+my @array = qw(the quick brown fox ran over the lazy dog); my $word;
+for $word (@array) {
+   print "$word ";
+}
+print "\n";
+
+my %hash = ( car => 'voiture', coach => 'car', bus => 'autobus' ); for (keys %hash) {
+print "English: $_\n";
+print "French: $hash{$_}\n\n"; }
+
+#!/usr/bin/perl
+# forloop5.plx
+use warnings;
+use strict;
+my @array = (1..10);
+foreach (@array) {
+$_++;
+}
+print "Array is now: @array\n";
+
+
+#!/usr/bin/perl
+ # quicksum.plx
+use warnings;
+use strict;
+my $total=0; 
+$total += $_ for @ARGV;
+print "The total is $total\n";
+
+#!/usr/bin/perl
+ # whatsargv.plx
+use warnings;
+use strict;
+foreach (@ARGV) {
+print "Element: |$_|\n"; 
+}
+
+# #!/usr/bin/perl
+#  # sdrawkcab1.plx
+# use warnings;
+# use strict;
+# while (<STDIN>) {
+#     chomp;
+#     die "!enod llA\n" unless $_; my $sdrawkcab = reverse $_;
+#     print "$sdrawkcab\n";
+# }
+
+#!/usr/bin/perl
+# sdrawkcab2.plx
+use warnings;
+use strict;
+do {
+    $_ = <STDIN>;
+    chomp;
+    my $sdrawkcab = reverse $_; print "$sdrawkcab\n";
+} while ($_);
+print "!enod llA\n";
+
+#!/usr/bin/perl
+# until.plx
+use warnings;
+use strict;
+my $countdown = 5;
+until ($countdown-- == 0) {
+print "Counting down: $countdown\n";
+}
+
+#!/usr/bin/perl
+# sdrawkcab3.plx
+use warnings;
+use strict;
+my $stopnow = 0;
+until ($stopnow) {
+    $_ = <STDIN>;
+    chomp;
+    if ($_) {
+        my $sdrawkcab = reverse $_; print "$sdrawkcab\n";
+    } 
+    else {
+        $stopnow = 1;
+    }
+}
+print "!enod llA\n";
+
+
+
+#!/usr/bin/perl
+# sdrawkcab4.plx
+use warnings;
+use strict;
+while (<STDIN>) {
+chomp;
+last unless $_;
+my $sdrawkcab = reverse $_;
+print "$sdrawkcab\n";
+}
+
+#!/usr/bin/perl
+# forlast.plx
+use warnings;
+use strict;
+my @array = ( "red", "blue", "STOP THIS NOW", "green");
+for (@array) {
+    last if $_ eq "STOP THIS NOW";
+    print "Today's colour is $_\n";
+}
+
+
+#!/usr/bin/perl
+ # next.plx
+use strict;
+use warnings;
+my @array = (8, 3, 0, 2, 12, 0);
+for (@array) {
+if($_ ==0){
+    print "Skipping zero element.\n";
+    next; }
+print "48 over $_ is ", 48/$_, "\n"; }
+
+
+# #!/usr/bin/perl
+# # looploop1.plx
+# use warnings;
+# use strict;
+# my @getout = qw(quit leave stop finish);
+# while (<STDIN>) {
+#     chomp;
+#     for my $check (@getout) {
+#     print "Testing $check against $_\n";
+#     last if $check eq $_;
+#     }
+#     print "Hey, you said $_\n";
+# }
+
+#!/usr/bin/perl
+# looploop3.plx
+use warnings;
+use strict;
+my @getout = qw(quit leave stop finish);
+OUTER: while (<STDIN>) {
+    chomp;
+    INNER: for my $check (@getout) {
+        last OUTER if $check eq $_;
+    }
+    print "Hey, you said $_\n"; 
+}
+
+
+
